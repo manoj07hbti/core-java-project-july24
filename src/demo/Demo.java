01b16c045d321224f8584f8d505e1fb672ea630b
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-07-22 21:40:51 +0530

    line delete demo

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
 
 
         System.out.println("This is my first java program..");
-        System.out.println("This is second line");
+
         System.out.println("This is third line");
 
 

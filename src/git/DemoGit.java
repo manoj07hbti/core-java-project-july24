6f1a93a2228db54633bdbafc1868125e9e33360f
basu khan <imrankhankspcivil00786@gmail.com>
2024-07-24 21:38:29 +0530

    this is my test commit1

--- src/git/DemoGit.java
+++ src/git/DemoGit.java
@@ -6,5 +6,7 @@ DemoGit {
     public static void main(String[] args) {
 
         System.out.println("thats my change...");
+        System.out.println("thats my new book");
+        System.out.println("how to new work");
     }
 }

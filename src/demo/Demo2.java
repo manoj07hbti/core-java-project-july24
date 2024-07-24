d48e357304bd4895dbe50e079df8a152fd04ce84
Vartusha03 <svartika45@gmail.com>
2024-07-24 21:47:29 +0530

    added new line in demo2

--- src/demo/Demo2.java
+++ src/demo/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
     public static void main(String[] args) {
 
         System.out.println("This is my second demo program...");
+        System.out.println(" new line added");
 
     }
 

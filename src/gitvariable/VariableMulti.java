a3428a48ebb9f39dd2048291770dbc36977707ab
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 10:02:31 +0530

    Variable Multiplication Commit

---
+++ src/gitvariable/VariableMulti.java
@@ -0,0 +1,28 @@
+package gitvariable;
+
+public class VariableMulti {
+
+    public static void main(String[] args) {
+
+        int a=96;
+        int b=65;
+        int multi=a*b;
+        System.out.println("Printing multiplication : "+multi);
+
+        a=64;
+        b=36;
+        int result=a*b;
+        System.out.println("Printing multiplication : "+result);
+
+
+
+    }
+
+
+
+
+
+
+
+
+}

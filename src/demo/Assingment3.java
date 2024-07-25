1c8a2b7807665f18aae532eaa969f11fa11d4424
Vartusha03 <svartika45@gmail.com>
2024-07-25 20:10:26 +0530

    ASSIGNMENT3:REVISION

---
+++ src/demo/Assingment3.java
@@ -0,0 +1,75 @@
+package demo;
+
+public class Assingment3 {
+
+    public static void main(String[] args) {
+
+        int x = 9;
+        int y = 5;
+        int sum = x + y;
+        System.out.println("ADDING VALUES OF X AND Y:"+sum);
+
+        x = 14;
+        y = 26;
+        sum = x + y;
+        System.out.println("ADDING VALUES OF X AND Y:"+sum);
+
+        x = 9;
+        y = 5;
+        int sub = x-y;
+        System.out.println("SUBTRACTING VALUES OF X AND Y:"+sub);
+
+        x = 60;
+        y = 40;
+        sub = x-y;
+        System.out.println("SUBTRACTING VALUES OF X AND Y:"+sub);
+
+        x = 10;
+        y = 5;
+        int multi = x*y;
+        System.out.println("MULTIPLYING VALUES OF X AND Y:"+multi);
+
+        x = 10;
+        y = 10;
+        multi = x*y;
+        System.out.println("MULTIPLYING VALUES OF X AND Y:"+multi);
+
+        x = 10;
+        y = 5;
+        int div = x/y;
+        System.out.println("DIVIDING VALUES OF X AND Y:"+div);
+
+        x = 800;
+        y = 10;
+        div = x/y;
+        System.out.println("DIVIDING VALUES OF X AND Y:"+div);
+
+        x =46;
+        y = 5;
+        int mod = x%y;
+        System.out.println("MODULUS VALUES OF X AND Y:"+mod);
+
+        x =9;
+        y = 5;
+        mod = x%y;
+        System.out.println("MODULUS VALUES OF X AND Y:"+mod);
+
+        int i=40;
+        int square=i*i;
+        System.out.println("SQUARE OF 6:"+ square);
+
+        int g=10;
+        int sq=g*g;
+        System.out.println("SQUARE OF 89:"+  sq);
+
+        int p= 30;
+        int cube = p*p*p;
+        System.out.println("CUBE OF 10:"+ cube);
+
+        p = 20;
+        cube = p*p*p;
+        System.out.println("CUBE OF 50:"+ cube);
+
+    }
+
+}

6d487958ba018ced06e6d27415d7f545e2da1246
Vartusha03 <svartika45@gmail.com>
2024-07-25 15:05:06 +0530

    CHARACTER DATATYPE CLASS

---
+++ src/demo/CharacterDatatype.java
@@ -0,0 +1,29 @@
+package demo;
+
+public class CharacterDatatype {
+
+    public static void main(String[] args) {
+
+        char alphabet1 = 'V';
+        System.out.println("LETTERS OF NAME VARTIKA:"+ alphabet1);
+
+        char alphabet2 = 'A';
+        System.out.println("LETTERS OF NAME VARTIKA:"+ alphabet2);
+
+        char alphabet3 = 'R';
+        System.out.println("LETTERS OF NAME VARTIKA:"+ alphabet3);
+
+        char alphabet4 = 'T';
+        System.out.println("LETTERS OF NAME VARTIKA:"+ alphabet4);
+
+        char alphabet5 = 'I';
+        System.out.println("LETTERS OF NAME VARTIKA:"+ alphabet5);
+
+        char alphabet6 = 'K';
+        System.out.println("LETTERS OF NAME VARTIKA:"+ alphabet6);
+
+        char alphabet7 = 'A';
+        System.out.println("LETTERS OF NAME VARTIKA:"+ alphabet7);
+
+    }
+}

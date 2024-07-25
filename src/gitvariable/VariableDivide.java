2951ec7eb6e0f50c7e4ac86d24117b90f7ad8a23
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 10:11:07 +0530

    Variable Divide Commit

---
+++ src/gitvariable/VariableDivide.java
@@ -0,0 +1,30 @@
+package gitvariable;
+
+public class VariableDivide {
+
+    public static void main(String[] args) {
+
+        int a=3;
+        int b=6;
+        double result= (double)a/b;
+        System.out.println("printing divide : "+result);
+
+        a=65;
+        b=75;
+        double divide=(double)a/b;
+        System.out.println("Printing divide : "+divide);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}

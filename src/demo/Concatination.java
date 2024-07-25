058b331c58ec8ff28495910210570320f6e0721e
Vartusha03 <svartika45@gmail.com>
2024-07-25 18:22:06 +0530

    CONCATINATION CLASS

---
+++ src/demo/Concatination.java
@@ -0,0 +1,29 @@
+package demo;
+
+public class Concatination {
+
+    public static void main(String[] args) {
+
+
+        String subject1 = "ENGLISH";
+        System.out.println("SUBJECT 1:" + subject1);
+
+        String subject2 = "MATHS";
+        System.out.println("SUBJECT 2:" + subject2);
+
+        String subject3 = "HINDI";
+        System.out.println("SUBJECT 3:" + subject3);
+
+        int marks1 = 79;
+        System.out.println("ENGLISH MARKS:"+ marks1);
+
+        int marks2 = 80;
+        System.out.println("MATHS MARKS:"+ marks2);
+
+        int marks3 = 92;
+        System.out.println("HINDI MARKS:"+ marks3);
+
+        System.out.println("SUBJECT 1:" + subject1 + " : " + "SUBJECT 2:" + subject2 + " : " + "SUBJECT 3:" + subject3);
+        System.out.println("ENGLISH MARKS:"+ marks1 + " : " + "MATHS MARKS:"+ marks2 + " : " + "HINDI MARKS:"+ marks3);
+    }
+}

3e657db3360d1df3d6ec8748fd4e6e6d87fde526
Vartusha03 <svartika45@gmail.com>
2024-07-25 19:54:45 +0530

    LINE DELETED IN THE DEMO CLASS

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -4,11 +4,7 @@ public class Demo {
 
     public static void main(String args[]) {
 
-
         System.out.println("This is my first java program..");
-
-        System.out.println("This is third line");
-
 
     }
 

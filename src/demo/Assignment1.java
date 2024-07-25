c3d9b257fc86bf1c16ac8e08bde51fddca684658
Vartusha03 <svartika45@gmail.com>
2024-07-25 18:55:12 +0530

    ASSINGMENT NO.1-CUBE OF A NUMBER                -SQAURE OF A NUMBER                -PERCENTAGE OF A NUMBER

---
+++ src/demo/Assignment1.java
@@ -0,0 +1,46 @@
+package demo;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+
+       //SQUARE
+       int g=30;
+       int square=g*g;
+       System.out.println("SQUARE OF G:"+ square);
+
+        int h=20;
+        square=h*h;
+        System.out.println("SQUARE OF H:"+ square);
+
+        //CUBE
+        int r=9;
+        int cube=r*r*r;
+        System.out.println("CUBE OF R:"+ cube);
+
+         r=10;
+         cube=r*r*r;
+         System.out.println("CUBE OF R:"+ cube);
+
+         //PERCENTAGE
+        int w=2000;
+        int percent=5;
+        int answer=(w*percent)/100;
+        System.out.println("5% of 2000:"+ answer);
+
+        int u=9999;
+        int per=3;
+        answer= (u*per)/100;
+        System.out.println("3% of 9999:"+ answer);
+
+
+
+
+
+
+
+
+
+
+    }
+}

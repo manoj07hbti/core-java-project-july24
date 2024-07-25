cffc0fc4275a51a5ddd9c092827c7426a5925ab4
Vartusha03 <svartika45@gmail.com>
2024-07-25 13:33:49 +0530

    INTEGER DATATYPE CLASS

---
+++ src/demo/IntegerDatatype.java
@@ -0,0 +1,22 @@
+package demo;
+
+public class IntegerDatatype {
+
+    public static void main(String[] args) {
+
+        int age1 = 24;
+        System.out.println("AGE OF NAME 1:"+ age1);
+
+        int age2 = 26;
+        System.out.println("AGE OF NAME 2:"+ age2);
+
+        int age3 = 25;
+        System.out.println("AGE OF NAME 3:"+ age3);
+
+        int age4 = 27;
+        System.out.println("AGE OF NAME 4:"+ age4);
+
+        int age5 = 22;
+        System.out.println("AGE OF NAME 5:"+ age5);
+    }
+}

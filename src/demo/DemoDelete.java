8481aca9ed200447e8f6d029787d53aa70527115
Vartusha03 <svartika45@gmail.com>
2024-07-25 20:00:47 +0530

    REMOVED DEMODELETED CLASS

--- src/demo/DemoDelete.java
+++
@@ -1,8 +0,0 @@
-package demo;
-
-public class DemoDelete {
-    public static void main(String[] args) {
-        System.out.println("TRY TO DELETE CLASS ");
-        System.out.println("TO CHECK , COMMIT WORK PROPERLY");
-    }
-}

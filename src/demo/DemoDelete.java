9c186bf9b38cbf0daa7c656e38eaabe72a26e7ab
Vartusha03 <svartika45@gmail.com>
2024-07-25 19:59:27 +0530

    ADD DEMODELETE CLASS TO CHECK COMMIT

---
+++ src/demo/DemoDelete.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class DemoDelete {
+    public static void main(String[] args) {
+        System.out.println("TRY TO DELETE CLASS ");
+        System.out.println("TO CHECK , COMMIT WORK PROPERLY");
+    }
+}

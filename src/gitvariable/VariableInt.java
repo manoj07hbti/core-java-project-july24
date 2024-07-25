6cefdf939bd697d8a73ed88a135851f213e3dcca
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 09:42:01 +0530

    Int commit

---
+++ src/gitvariable/VariableInt.java
@@ -0,0 +1,22 @@
+package gitvariable;
+
+import java.sql.SQLOutput;
+
+public class VariableInt {
+
+    public static void main(String[] args) {
+
+        int age=65;
+        System.out.println("Printing age :"+age);
+        age=25;
+        System.out.println("Printing age :"+age);
+
+
+    }
+
+
+
+
+
+
+}

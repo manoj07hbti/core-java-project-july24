79caa180fd06d9b09e7d4017fec9dbe3d894e56c
Vartusha03 <svartika45@gmail.com>
2024-07-25 15:46:28 +0530

    TYPECASTING CLASS

---
+++ src/demo/TypeCasting.java
@@ -0,0 +1,34 @@
+package demo;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+
+        int a = 73;
+        int b = 5;                        //TYPECASTING:(DATATYPE)VARIABLE
+        double divide = (double) a/b;
+        System.out.println("DIVIDING A/B:"+ divide);
+
+        int x= 30;
+        x=x+20;
+        System.out.println("ADDING VALUES:"+ x);
+
+        int y=20;
+        y=y*20;
+        System.out.println("MULTIPLYING VALUES:"+y);
+
+        y=y-10;
+        System.out.println("SUBTRACTING VALUES:"+y);
+
+         a = 97;
+         b = 3;
+        divide = (double) a/b;
+        System.out.println("DIVIDING A/B:"+ divide);
+
+
+
+
+
+
+    }
+}

df0887a14a1aa8e9e0e6f2c2efddfa566cc4385c
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 09:33:32 +0530

    String commit

---
+++ src/gitvariable/VariableString.java
@@ -0,0 +1,21 @@
+package gitvariable;
+
+public class VariableString {
+
+    public static void main(String[] args) {
+
+        // I have to create Git variable commit
+
+        String name= "Anshul Khan";
+        System.out.println("Printing name : "+name);
+        name="Riyaz";
+        System.out.println("Printing name : "+name);
+
+
+
+    }
+
+
+
+
+}

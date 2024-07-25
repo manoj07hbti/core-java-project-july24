8af4b5ca1985b7dac8ac403596cead0b4e1f7ce7
Vartusha03 <svartika45@gmail.com>
2024-07-25 15:15:33 +0530

    VARIABLE DECLARATION CLASS

---
+++ src/demo/VariableDeclaration.java
@@ -0,0 +1,24 @@
+package demo;
+
+public class VariableDeclaration {
+
+    public static void main(String[] args) {
+
+        int a;
+        a=45;
+        System.out.println("DECLARING VALUE OF A:"+ a);
+
+        int b;
+        b=75;
+        System.out.println("DECLARING VALUE OF B:"+ b);
+
+        String city = "JAIPUR";
+        System.out.println("CAPITAL OF RAJASTHAN:"+ city);
+
+        String country = "INDIA";
+        System.out.println("COUNTRY NAME:"+ country);
+
+        double weight = 70.5;
+        System.out.println("WEIGHT:"+ weight);
+    }
+}

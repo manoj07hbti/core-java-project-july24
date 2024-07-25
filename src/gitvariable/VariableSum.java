ffd2a835f3da59c7288194328fb4f440962f7180
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 09:53:42 +0530

    Variable sum commit

---
+++ src/gitvariable/VariableSum.java
@@ -0,0 +1,26 @@
+package gitvariable;
+
+public class VariableSum {
+
+    public static void main(String[] args) {
+
+        int a=98;
+        int b=65;
+        int result=a+b;
+        System.out.println("Printing addition : "+result);
+
+        a=65;
+        b=32;
+        int resulta=a+b;
+        System.out.println("PRinting addition : "+resulta);
+
+
+
+    }
+
+
+
+
+
+
+}

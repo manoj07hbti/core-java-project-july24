4b8263c79c781ed43f3f04f806fcb47071a57d08
hpRashmi <rashmi.ktdl@gmail.com>
2024-07-25 01:31:15 -0700

    This is my commit second commit

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -6,7 +6,7 @@ public class Test {
 
         System.out.println("This file is created for GIT commit demo purpose..");
 
-         System.out.println("this is my first git program");
+         System.out.println("my secon git);
 
 
 

658d35bd087ed0be8d86dfdf9966a34a265e4026
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 18:42:51 +0530

    Variable Percentage Commit

---
+++ src/gitvariable/VariablePercentageCalculate.java
@@ -0,0 +1,35 @@
+package gitvariable;
+
+public class VariablePercentageCalculate {
+
+    public static void main(String[] args) {
+
+        int number = 965;
+        int percentage =10;
+        double result=(double) (number*percentage)/100;
+        System.out.println("Printing percentage : "+result);
+
+        number= 65489;
+        percentage=65;
+        double r=(double)(number*percentage)/100;
+        System.out.println("Printing percentage : "+r);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}

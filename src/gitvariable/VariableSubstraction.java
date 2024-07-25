2c555489f8b42d89beecfb79e66461e3ad755f73
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 09:57:55 +0530

    Variable substraction commit

---
+++ src/gitvariable/VariableSubstraction.java
@@ -0,0 +1,27 @@
+package gitvariable;
+
+public class VariableSubstraction {
+
+    public static void main(String[] args) {
+
+        int a=65;
+        int b=45;
+        int result=a-b;
+        System.out.println("Printing substraction : "+result);
+
+        a=69;
+        b=35;
+        int r=a-b;
+        System.out.println("PRinting substraction : "+r);
+
+
+
+
+    }
+
+
+
+
+
+
+}

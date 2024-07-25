a33753078f65fca28758d1e2df8f3d3803d1ef34
hpRashmi <rashmi.ktdl@gmail.com>
2024-07-25 01:49:24 -0700

    this is my git program commit

---
+++ src/demo/gitdemo.java
@@ -0,0 +1,16 @@
+package demo;
+
+public class demogit {
+
+    public static void main(String[] args) {
+
+        System.out.println("printing git program");
+        System.out.println("printing test git java program");
+
+
+
+    }
+
+
+
+}

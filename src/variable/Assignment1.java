b4f7f51e526545cf75a5182d6fc5518513acd9fb
anshul19912 <anshulsingh19912@gmail.com>
2024-07-25 08:41:51 +0530

    assignment1 added

---
+++ src/variable/Assignment1.java
@@ -0,0 +1,22 @@
+package variable;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+
+        int num= 5;
+
+        System.out.println("square of " +num +" is : " +num*num);
+        System.out.println("cube of " +num +" is : " +num*num*num);
+
+        int x =3, y=5;
+
+        double result = (double) x/y;
+
+        System.out.println("Addition of x and y : " + (x+y));
+        System.out.println("subtraction of x and y : " + (x-y));
+        System.out.println("product of x and y : " + (x*y));
+        System.out.println("division of x and y : " +result);
+
+    }
+}

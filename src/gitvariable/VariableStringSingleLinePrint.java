bddcf2a34b3e0355db513a1ab5aa2e89f89836bb
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 10:20:47 +0530

    Variable Single Line String Commit

---
+++ src/gitvariable/VariableStringSingleLinePrint.java
@@ -0,0 +1,32 @@
+package gitvariable;
+
+public class VariableStringSingleLinePrint {
+
+    public static void main(String[] args) {
+
+        String name1="Faizan";
+        System.out.println("Printing name : "+name1);
+        String name2="Ishank";
+        System.out.println("Printing name : "+name2);
+        String name3="Saif";
+        System.out.println("Printing name : "+name3);
+
+        System.out.println("Printing name : "+name1 + "PRinting name : "+name2 + "Printing name : "+name3);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}

6e3648f2a132ddbb294224b05cefb00eaed4b21d
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 09:47:00 +0530

    Double commit

---
+++ src/gitvariable/VariableDouble.java
@@ -0,0 +1,20 @@
+package gitvariable;
+
+public class VariableDouble {
+
+    public static void main(String[] args) {
+
+        double price=56.32;
+        System.out.println("Printing double : "+price);
+        price=65.3265;
+        System.out.println("Printing double : "+price);
+
+
+
+    }
+
+
+
+
+
+}

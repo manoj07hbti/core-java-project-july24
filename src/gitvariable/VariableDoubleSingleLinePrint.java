63b7d9352b18aec376d19f2f700bf1cacf069e52
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 18:13:29 +0530

    Variable Single Line Double Commit

---
+++ src/gitvariable/VariableDoubleSingleLinePrint.java
@@ -0,0 +1,41 @@
+package gitvariable;
+
+public class VariableDoubleSingleLinePrint {
+
+    public static void main(String[] args) {
+
+        double price1= 65.32;
+        double price2= 56.32;
+        double result=price1+price2;
+        System.out.println("Printing : "+result);
+
+        System.out.println("Printing : "+price1 + "printing : "+price2);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

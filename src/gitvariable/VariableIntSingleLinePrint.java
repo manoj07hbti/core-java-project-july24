3125649af9cfbf0d831f1f99f30322b4c219c93e
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 10:33:08 +0530

    Variable Single Line Int Commit

---
+++ src/gitvariable/VariableIntSingleLinePrint.java
@@ -0,0 +1,33 @@
+package gitvariable;
+
+public class VariableIntSingleLinePrint {
+
+    public static void main(String[] args) {
+
+        int a1=56;
+        int b1=89;
+        int add1=a1+b1;
+        System.out.println("Printing adition : "+add1);
+
+        int a2=98;
+        int b2=56;
+        int add2=a2+b2;
+        System.out.println("Prinitng addition : "+add2);
+
+        System.out.println("Printing addition : "+add1 + "Printing addition : "+add2);
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}

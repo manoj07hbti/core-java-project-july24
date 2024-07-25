cb2a85bdf4d6fb3d35433cda199ed397ce969bfb
Vartusha03 <svartika45@gmail.com>
2024-07-25 13:26:48 +0530

    STRING DATATYPE CLASS

---
+++ src/demo/StringDatatype.java
@@ -0,0 +1,23 @@
+package demo;
+
+public class StringDatatype {
+
+    public static void main(String[] args) {
+
+        String name1 = "VARTIKA";
+        System.out.println("NAME 1:"+ name1);
+
+        String name2 = "HEMANI";
+        System.out.println("NAME 2:"+ name2);
+
+        String name3 = "ARUN";
+        System.out.println("NAME 3:"+ name3);
+
+        String name4 = "GANESH";
+        System.out.println("NAME 4:"+ name4);
+
+        String name5 = "VAIBHAV";
+        System.out.println("NAME 5:"+ name5);
+
+    }
+}

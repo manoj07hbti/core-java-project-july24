f44c485541c134c3a415bf84d227bdbfc9fea5b4
Vartusha03 <svartika45@gmail.com>
2024-07-25 13:40:58 +0530

    DOUBLE DATATYPE CLASS

---
+++ src/demo/DoubleDatatype.java
@@ -0,0 +1,23 @@
+package demo;
+
+public class DoubleDatatype {
+
+    public static void main(String[] args) {
+
+        double percent1 = 90.6;
+        System.out.println("PERCENTAGE OF NAME 1:"+ percent1 +"%");
+
+        double percent2 = 88.6;
+        System.out.println("PERCENTAGE OF NAME 2:"+ percent2 +"%");
+
+        double percent3 = 45.6;
+        System.out.println("PERCENTAGE OF NAME 3:"+ percent3 +"%");
+
+        double percent4 = 92.5;
+        System.out.println("PERCENTAGE OF NAME 4:"+ percent4 +"%");
+
+        double percent5 = 94.5;
+        System.out.println("PERCENTAGE OF NAME 5:"+ percent5 +"%");
+
+    }
+}

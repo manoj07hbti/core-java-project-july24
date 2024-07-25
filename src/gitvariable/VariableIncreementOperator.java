78aaf466416692da764510354efe30ccfc7acbcd
Anshul152 <anshulkhan71@gmail.com>
2024-07-25 18:33:02 +0530

    Variable Increement decreement Commit

---
+++ src/gitvariable/VariableIncreementOperator.java
@@ -0,0 +1,40 @@
+package gitvariable;
+
+public class VariableIncreementOperator {
+
+    public static void main(String[] args) {
+
+        // Increement operator
+
+        int
+                i=1;
+        System.out.println("Printing before increement operator : "+i);
+
+        i++;
+        System.out.println("PRinting after increement operator : "+i);
+
+        // Decreement operator
+
+        i=1;
+        System.out.println("Printing before decreement operator : "+i);
+        i--;
+        System.out.println("Printing after decreement operator : "+i);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}

4bac857706eae8fa1a33ba77fbe39e1852ecb606
basu khan <imrankhankspcivil00786@gmail.com>
2024-07-26 11:15:24 +0530

    this is student project1

---
+++ src/demo/Studentclass.java
@@ -0,0 +1,51 @@
+package demo;
+
+public class Studentclass {
+
+
+
+    public static void main(String args []) {
+
+            // write a code to add two number
+
+            int add=2+3;
+            System.out.println("Add is "+add);
+
+            int a=3;
+            int b=7;
+            int sum=a+b;
+            System.out.println("Addition is :"+sum);
+
+            //write a code to subtracts two number
+            int sub=a-b;
+
+            System.out.println("Subtraction is : "+sub);
+
+            // write a code to multiply two numbers
+            int multi= a*b;
+            System.out.println("Printing Multiplication "+multi);
+
+            // (datatype) variable/expression
+
+            double divide=(double) a/b; //
+            System.out.println("Division is: "+divide);
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+

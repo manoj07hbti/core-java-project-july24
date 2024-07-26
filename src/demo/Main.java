cdc22ce35e68d3827fbd28a92653858a03ee19c6
basu khan <imrankhankspcivil00786@gmail.com>
2024-07-26 10:28:05 +0530

    this is new project today for my new project

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -1,7 +1,51 @@
 package demo;
 
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+    public static void main(String[] args){
+
+        //SYNTAX:datatype varNAme://we have declared variable ;
+
+        String name;//we have declaerd variable with my name Bashu as name//;
+
+        name="bashu i am blong to nagla ram kishan line par tundla";
+
+        System.out.println("printing name is" +name);
+
+        name="imran";//reuse
+
+        System.out.println("after reuse name is" + name);
+
+        long age;//we have declared age variable with long datatype to store complete numbers;
+
+        age=26;//we have store age 26;
+
+        System.out.println("printing long" + age);
+
+        age=45;//reuse
+
+        System.out.println("after reuse long is " +age);
+
+
+        double price;//we have created variable name price dobule datatype to store decimal number;
+
+        price=34.65;//we have store price 34.65.
+
+        System.out.println("printing double" +price);
+
+        price=87.45;//reuse
+
+        System.out.println("after reuse double" +price);
+
+
+
+
+
+
+
+
     }
 }

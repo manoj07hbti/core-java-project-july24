f48693f1271cf9af3ae28d071dcae6ac34c2b458
Anshul152 <anshulkhan71@gmail.com>
2024-07-26 10:06:33 +0530

    Card Commit

--- src/gitvariable/VariableString.java
+++ src/gitvariable/VariableString.java
@@ -6,16 +6,24 @@ public class VariableString {
 
         // I have to create Git variable commit
 
-        String name= "Anshul Khan";
-        System.out.println("Printing name : "+name);
-        name="Riyaz";
-        System.out.println("Printing name : "+name);
+        String name = "Anshul Khan";
+        System.out.println("Printing name : " + name);
+        name = "Riyaz";
+        System.out.println("Printing name : " + name);
 
+        String number = "ENPPK9802J";
+        System.out.println("Printing card number : " + number);
+
+        number = "462956612315";
+        System.out.println("Printing aadhar number :" + number);
+
+        String date = "05 - 11 - 1998";
+        System.out.println("Printing date of birth : " + date);
 
 
     }
 
 
-
-
 }
+
+

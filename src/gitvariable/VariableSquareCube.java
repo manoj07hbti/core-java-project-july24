ca845347cb091916ebd6395ac934cad93b06f7b0
Anshul152 <anshulkhan71@gmail.com>
2024-07-26 09:05:11 +0530

    Square And Cube Commit

---
+++ src/gitvariable/VariableSquareCube.java
@@ -0,0 +1,38 @@
+package gitvariable;
+
+public class VariableSquareCube {
+
+    public static void main(String[] args) {
+
+        int x=89;
+        int square=x*x;
+        System.out.println("Printing square : "+square);
+
+        x=16;
+        int s=x*x;
+        System.out.println("Printing sqaure : "+s);
+
+        int a= 3;
+        int cube= a*a*a;
+        System.out.println("Printing cube : "+cube);
+
+        a=56;
+        int c= a*a*a;
+        System.out.println("Printing cube :"+c);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}

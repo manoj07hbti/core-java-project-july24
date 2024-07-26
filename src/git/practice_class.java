ca92102e219b31d7ee3ad1950d30f5288d22ed51
Himanshu <mannukumar9557@gmail.com>
2024-07-26 20:34:22 +0530

    revision commit again

---
+++ src/git/practice_class.java
@@ -0,0 +1,23 @@
+package git;
+
+public class practice_class {
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("this is java word");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}

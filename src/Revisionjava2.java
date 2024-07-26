8cb38500f651e3de9de5727f86324f156d27b6f4
Himanshu <mannukumar9557@gmail.com>
2024-07-26 20:21:51 +0530

    revision commit

--- src/Revisionjava2.java
+++ src/Revisionjava2.java
@@ -4,8 +4,9 @@ public class Revisionjava2 {
     public static void main(String[] args) {
 
 
-        System.out.println("this is git revision");
+        System.out.println("this is hello word");
         System.out.println("ths is real git word");
+
 
     }
 

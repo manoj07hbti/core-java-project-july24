63b8981947e6b6c723220005febd5fb16fa833ec
Anshul152 <anshulkhan71@gmail.com>
2024-07-26 08:57:57 +0530

    Modulus Operator Commit

---
+++ src/gitvariable/VariableModulusOperator.java
@@ -0,0 +1,36 @@
+package gitvariable;
+
+public class VariableModulusOperator {
+
+
+    public static void main(String[] args) {
+
+        int x=6;
+        int y=5;
+        int modulus=x%y;
+        System.out.println("Pinting modulus : "+modulus);
+
+        x=81;
+        y=3;
+        int mod= x%y;
+        System.out.println("Printing modulus : "+mod);
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}

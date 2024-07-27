f975d03c7e76408980346c37b4a321f5c8acf09b
basu khan <imrankhankspcivil00786@gmail.com>
2024-07-27 14:26:03 +0530

    add my commit for tall

---
+++ src/conditional_statements/Tall_demo.java
@@ -0,0 +1,24 @@
+package conditional_statements;
+
+public class Tall_demo {
+
+
+    public static void main(String args[]) {
+
+
+        // SYNTAX : if(expression/condition){
+        //           CODE   }
+
+        int tall = 05;
+
+        if (tall > 06) {
+            System.out.println("this boy hight is " + tall);
+        }
+
+        if (tall < 05) {
+            System.out.println("this boy hight is not" + tall);
+
+
+        }
+    }
+}

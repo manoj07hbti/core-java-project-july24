4fad3599d52a075ad7c821aab40bafef554ff9c6
basu khan <imrankhankspcivil00786@gmail.com>
2024-07-27 14:16:26 +0530

    add my commit for dept of science

---
+++ src/conditional_statements/Science_dept_demo.java
@@ -0,0 +1,30 @@
+package conditional_statements;
+
+public class Science_dept_demo {
+
+    public static void main(String args []){
+
+
+        // String: condition
+
+
+        String department = "science";
+
+        if (department=="science"){
+
+            System.out.println("this person is from " +department );
+
+            if(department !="science");
+
+            System.out.println("this boy is not saction" +department);
+
+
+            System.out.println("this persion is not "+department);
+        }
+    }
+
+
+
+
+
+}

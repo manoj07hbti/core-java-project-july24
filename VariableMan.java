a0d7897ce0b7fa1ad606e6436ddd521b38ca09b4
hpRashmi <rashmi.ktdl@gmail.com>
2024-07-28 08:43:40 -0700

    my test 28 july java program variableMan

---
+++ VariableMan.java
@@ -0,0 +1,20 @@
+public class VariableMan {
+
+    public static void main(String[] args) {
+
+    //Write a code to add  two number
+
+    int x=4;
+    int y=5;
+    int sum=x+y;
+
+    System.out.printing("Addition is :"+sum);
+
+
+
+    }
+
+
+
+
+}

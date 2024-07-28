4b6423c31c14e9c4bcdccaabab44c33d1ae9832a
Anshul152 <anshulkhan71@gmail.com>
2024-07-28 21:24:48 +0530

    VARIABLE TEST COMMIT

---
+++ src/variable/VariableTestJuly28.java
@@ -0,0 +1,68 @@
+package variable;
+
+public class VariableTestJuly28 {
+
+    public static void main(String[] args) {
+
+      String number="462956610135";
+      String city="KASHIPUR";
+      String name="ANSHUl";
+      System.out.println("Printing aadhar..."+number + "Printing city..."+city + "Printing name.."+name);
+
+
+      int x=1;
+      int y=2;
+      int sum=x+y;
+      System.out.println("Sum.."+sum);
+
+      int multi=x*y;
+      System.out.println("Multi.."+multi);
+
+      int sub=x-y;
+      System.out.println("Substraction.."+sub);
+
+      double divide= (double)x/y;
+      System.out.println("Divide.."+divide);
+       // MODULUS
+
+      int a=5;
+      int b=4;
+      int mod= x%y;
+      System.out.println("Modulus..."+mod);
+
+      // Decreement operator
+
+        int i=1;
+        i--;
+        System.out.println("decreement.."+i);
+
+        int j=2;
+        j--;
+        System.out.println("decreement.."+j);
+
+        // Increement operator
+
+        int x1=1;
+        x1++;
+        System.out.println("Increement.."+x1);
+
+        int y1=2;
+        y1++;
+        System.out.println("Increement.."+y1);
+
+        // CODE START PATTERN
+
+        for(int c=1; c<=5; c++) {
+            System.out.println("c = "+c);
+        }
+
+
+
+
+
+    }
+
+
+
+
+}

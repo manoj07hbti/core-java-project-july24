87ebb4a1d7a4ea090ece05660533bc7fcf43f646
anshul19912 <anshulsingh19912@gmail.com>
2024-07-28 20:45:40 +0530

    test_28_july

--- src/conditional_statements/Assignment1.java
+++ src/conditional_statements/Assignment1.java
@@ -1,2 +1,61 @@
-package conditional_statements;public class Assignment1 {
-}
+package conditional_statements;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+
+        //1- check voting eligibility
+        int age=17;
+
+        if(age>18){
+            System.out.println("Welcome to Voting System");
+        } else{
+            System.out.println("You're not eligible for Voting as your age is less than 18 years.");
+        }
+
+        //2- marks
+        int marks=45;
+
+        if(marks>33){
+            System.out.println("Clear the Exam");
+        }else{
+            System.out.println("Not Cleared the Exam");
+        }
+
+        //3-Nationality check
+        String country= "INDIA";
+
+        if(country == "INDIA"){
+            System.out.println("INDIAN");
+        }else{
+            System.out.println("Not INDIAN");
+        }
+
+        //4- Science
+        String dept="COMMERCE";
+
+        if(dept == "SCIENCE"){
+            System.out.println("SCIENCE DEPARTMENT STUDENT");
+        }else{
+            System.out.println("NOT SCIENCE DEPARTMENT STUDENT");
+        }
+
+        //5- Height Check
+        double height= 5.11;
+
+        if(height>6){
+            System.out.println("Candidate is Tall");
+        }else{
+            System.out.println("Candidate is not Tall");
+        }
+
+
+
+
+
+
+        }
+
+    }
+
+
---
+++ src/test_28_july/Test.java
@@ -0,0 +1,77 @@
+package test_28_july;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        //1
+        long aadhaarnum = 893140648445L;
+        String name= "Anshul Singh";
+        String city= "Agra";
+
+        System.out.println("Name - "+name +" Aadhaar number- " +aadhaarnum + " city- " +city);
+
+        //2
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        //3
+        long num =  35632231111123L;
+
+        long result =  (num*7)/100;
+        System.out.println("7% of 35632231111123 is: " +result);
+
+        //4
+        int x=8, y=3;
+
+        System.out.println("Remainder of 8 and 3 is: " +x%y);
+
+        //5
+        x= 45;
+        y =66;
+
+        x++;
+        y++;
+
+        System.out.println("Value of x after increment is: " +x);
+        System.out.println("Value of y after increment is: " +y);
+
+
+        //6
+        int i= 10;
+        int j= 20;
+
+        i--;
+        j--;
+
+        System.out.println("Value of i after decrement is: " +i);
+        System.out.println("Value of j after decrement is: " +j);
+
+        //7
+         x=10;
+         y=2;
+
+        int add= x+y;
+        int sub= x-y;
+        int mul= x*y;
+        int div= x/y;
+
+        System.out.println("Addition of x and y: " +add);
+        System.out.println("Subtraction of x and y: " +sub);
+        System.out.println("Product  of x and y: " +mul);
+        System.out.println("Division of x and y: " +div);
+
+
+
+
+
+
+
+
+
+    }
+
+}

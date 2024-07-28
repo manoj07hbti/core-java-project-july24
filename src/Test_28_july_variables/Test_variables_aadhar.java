3f44e59e32b56a75c0388c2ef84ea3afc5f7f985
basu khan <imrankhankspcivil00786@gmail.com>
2024-07-28 20:42:13 +0530

    city commit

---
+++ src/Test_28_july_variables/Test_variables_aadhar.java
@@ -0,0 +1,23 @@
+package Test_28_july_variables;
+
+public class Test_variables_aadhar {
+
+    public static void main(String args [] ){
+
+     //SYNTAX:  datatype varNAme=value;
+
+        String name = "bashu khan from nagla ram kishan line par tundla";
+
+        System.out.println("printing name" +name);
+
+
+
+
+
+
+
+    }
+
+
+
+}

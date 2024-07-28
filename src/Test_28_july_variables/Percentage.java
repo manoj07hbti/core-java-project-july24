8e6329689cdcda2770a281f71d5ed4a5cbb15fdd
basu khan <imrankhankspcivil00786@gmail.com>
2024-07-28 21:00:35 +0530

    percentage commit

---
+++ src/Test_28_july_variables/Percentage.java
@@ -0,0 +1,18 @@
+package Test_28_july_variables;
+
+public class Percentage {
+
+
+    public static void main(String args [])){
+
+    //SYNTAX; dobule result=(dobule) (number*percentage)/100
+
+    long number = 3563223;
+    long percentage =7;
+
+    double result = (double)(number*percentage)/100;
+
+    System.out.println("printing percentage  :"+result);
+
+    }
+}
---
+++ src/Test_28_july_variables/Star.java
@@ -0,0 +1,11 @@
+package Test_28_july_variables;
+
+public class Star {
+    public static void main(String args [] ){
+
+
+
+
+
+    }
+}

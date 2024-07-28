0f88ccedbc4048f3115067d2806d259addb9a17c
hpRashmi <rashmi.ktdl@gmail.com>
2024-07-28 08:04:46 -0700

    this is my git program commit

---
+++ Test_28_July.java
@@ -0,0 +1,23 @@
+public class Test_28_july {
+
+    public static void main(String[] args) {
+
+        String name;
+        name="Rahul" System.out.printing("printing name "+name);
+
+        int Adhar no;
+        Adharno=3456876526;  System.out.printig("printing adharno"+no);
+
+        String city candidate;
+        city candidate="pune";  System.out.printing("printing city"+city candidate);
+
+ System.out.printing("printing name"+name+"prenting adharcardno"+no+"printing city"+city candidate)
+
+
+
+
+    }
+
+
+
+}

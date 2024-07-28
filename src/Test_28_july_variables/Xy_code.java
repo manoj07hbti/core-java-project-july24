56d64879a655de32b0c0a3d0ba576989ec808d9f
basu khan <imrankhankspcivil00786@gmail.com>
2024-07-28 21:08:17 +0530

    x and y commit

---
+++ src/Test_28_july_variables/Xy_code.java
@@ -0,0 +1,21 @@
+package Test_28_july_variables;
+
+public class Xy_code {
+
+    public static void main(String args [] ){
+
+        int x=6;
+        int y=8;
+
+          int sum= x+y;
+
+        System.out.println("printing int" +sum);
+
+
+
+
+
+
+
+    }
+}

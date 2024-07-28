a5f5a97d35c125d4e4f17dc507556215179d5278
basu khan <imrankhankspcivil00786@gmail.com>
2024-07-28 21:41:34 +0530

    aadhar

--- src/Test_28_july_variables/Test_variables_aadhar.java
+++ src/Test_28_july_variables/Test_variables_aadhar.java
@@ -8,7 +8,7 @@ public class Test_variables_aadhar {
         String name="bashu";
         System.out.println("printing name" +name);
         String number="231277872161";
-        String city="nagla ramkishan line pr tundla";
+        String city="nagla ramkishan line pr tundla firozabad";
 
         System.out.println("Printing aadhar..."+number + "Printing city..."+city + "Printing name.."+name);
 

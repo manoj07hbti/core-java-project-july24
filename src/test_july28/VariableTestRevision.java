4969a0904e57448c1d31f6048d18b688a67678ba
Anshul152 <anshulkhan71@gmail.com>
2024-07-29 08:21:10 +0530

    VARIABLE TEST REVISON

---
+++ src/test_july28/VariableTestRevision.java
@@ -0,0 +1,83 @@
+package test_july28;
+
+public class VariableTestRevision {
+
+    public static void main(String[] args) {
+
+        // 1
+          String name="ANSHUL KHAN";
+          System.out.println("Printing name...."+name);
+          String city="KASHIPUR";
+          System.out.println("Printing city...."+city);
+          String number="462956610135";
+          System.out.println("PRinting number...."+number);
+
+          // 2
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        // 3
+
+        int a=5;
+        int b=4;
+        int modulus=a%b;
+        System.out.println("Modulus.."+modulus);
+
+        // 4
+
+        int x=56;
+        int y=75;
+        System.out.println("Printing before aplying increement..."+x + ":"+y);
+        x++;
+        y++;
+        System.out.println("Printing after applying increement "+x + ":"+y);
+
+        // 5
+
+        int i=25;
+        int j=35;
+        System.out.println("Printing before applying decreement..."+i + ":"+j);
+        i--;
+        j--;
+        System.out.println("Printing after applying decreement...."+i + ":"+j);
+
+        // 6
+
+        int X=25;
+        int Y=26;
+        int sum= X+Y;
+        System.out.println("Addition... "+sum);
+
+        int sub= X-Y;
+        System.out.println("Substraction..."+sub);
+
+        int multi= X*Y;
+        System.out.println("Multiplication..."+multi);
+
+        double divide= (double)X/Y;
+        System.out.println("Divide..."+divide);
+
+        // 7
+
+        long num= 35632231111123L;
+        double percent= 0.07;
+        double result= num*percent;
+        System.out.println("Percentage..."+result);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}

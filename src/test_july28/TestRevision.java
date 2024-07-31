aa18cc7128ef07492558b319e03e9f3e1417bb80
Anshul152 <anshulkhan71@gmail.com>
2024-07-31 09:46:12 +0530

    TEST REVISION

---
+++ src/test_july28/TestRevision.java
@@ -0,0 +1,79 @@
+package test_july28;
+
+public class TestRevision {
+
+    public static void main(String[] args) {
+
+        /// 1
+
+        String number="462956610135";
+        String name="ANSHUL KHAN";
+        String city="KASHIPUR";
+        System.out.println("Printing number.."+number + "printing name.. "+name + "Printing city..."+city);
+
+        /// 2
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+
+        /// 3
+
+        long nu=35632231111123L;
+        double percent= 7;
+        double result= (double)(nu*percent)/100;
+        System.out.println("PRinting percentage..."+result);
+
+        ///  4
+
+        int x=25;
+        int y= 4;
+        int mod=x%y;
+        System.out.println("remainder..."+mod);
+
+        /// 5
+
+        int X= 26;
+        int Y=27;
+        System.out.println("Printing before applying increement..."+X + ";" +Y);
+        X++;
+        Y++;
+        System.out.println("Printing after applying increement operator..."+X + ";" +Y);
+
+        /// 6
+
+        int i=3;
+        int j=4;
+        i--;
+        j--;
+        System.out.println("decreement..."+i + ";" +j);
+
+        /// 7
+         int X1= 5;
+         int Y1= 6;
+         int sum= X1+Y1;
+         System.out.println("Addition..."+sum);
+
+         int substraction= X1-Y1;
+         System.out.println("Substraction..."+substraction);
+
+         int multi= X1*Y1;
+         System.out.println("Multiplication..."+multi);
+
+         double divide= (double)X1/Y1;
+         System.out.println("divide..."+divide);
+
+
+
+
+    }
+
+
+
+
+
+
+
+}

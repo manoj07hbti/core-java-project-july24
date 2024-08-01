4eb31b8a9f0e05b3584379e4d251d7bcd3476764
Vartusha03 <svartika45@gmail.com>
2024-08-01 13:41:58 +0530

    AND OPERATOR:EXAMPLE1:60 && SCIENCE              EXAMPLE2:18 && UP

--- src/conditional_statements/AndOperatorExample.java
+++ src/conditional_statements/AndOperatorExample.java
@@ -1,6 +1,6 @@
 package conditional_statements;
 
-public class AndOperator {
+public class AndOperatorExample {
 
     public static void main(String[] args) {
 
@@ -47,6 +47,53 @@ public class AndOperator {
             System.out.println("student got 60% marks in science stream");
         }else {
             System.out.println("student do not obtained cut of marks for science stream");
+        }
+
+        //example2)code for to find eligible voter of UP
+
+        //AND OPERATOR
+        //Condition First--> 1*1 = 1
+
+        int age=18;
+        String city="UP";
+
+        if(age>=18 && city=="UP"){
+            System.out.println("candidate is eligible for vote in UP");
+        }else {
+            System.out.println("candidate is not eligible for vote in UP");
+        }
+
+        //condition second-->1*0 = 0
+
+        age=19;
+        city="DELHI";
+
+        if(age>=18 && city=="UP"){
+            System.out.println("candidate is eligible for vote in UP");
+        }else {
+            System.out.println("candidate is not eligible for vote in UP");
+        }
+
+        //condition third-->0*1 = 0
+
+        age=16;
+        city="UP";
+
+        if(age>=18 && city=="UP"){
+            System.out.println("candidate is eligible for vote in UP");
+        }else {
+            System.out.println("candidate is not eligible for vote in UP");
+        }
+
+        //condition fouth-->0*0 = 0
+
+        age=14;
+        city="DELHI";
+
+        if(age>=18 && city=="UP"){
+            System.out.println("candidate is eligible for vote in UP");
+        }else {
+            System.out.println("candidate is not eligible for vote in UP");
         }
     }
 }

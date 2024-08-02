8460757a24b9db22a5a51cbe32000be2b902f35d
Himanshu <mannukumar9557@gmail.com>
2024-08-02 12:48:39 +0530

    if and else assisment

---
+++ src/condition_statements/Myifandelse.java
@@ -0,0 +1,54 @@
+package condition_statements;public class Myifandelse {
+
+
+    public static void main(String[] args) {
+
+
+
+        int age=17;
+        if(age>18){
+            System.out.println("welcome to voting system....."+age);
+
+        }  else {
+            System.out.println("you are not elligble for voting");
+        }
+
+
+        int marks=60;
+        if(marks>48){
+            System.out.println("first division...."+marks);
+        } else {
+            System.out.println("not cleared exam...."+marks);
+        }
+
+
+
+
+        int price=5000;
+        if(price<4800){
+            System.out.println("price is costly..."+price);
+        } else {
+            System.out.println("price is avarage...."+price);
+        }
+
+
+
+        String country= "indian";
+        if(country=="dubai"){
+            System.out.println("you are indian");
+        } else {
+            System.out.println("you are not indian...");
+        }
+
+
+
+
+        }
+
+    }
+
+
+
+
+
+}

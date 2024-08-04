fa39b880c3a30fdc8b2a778bf8724abaca728415
hpRashmi <rashmi.ktdl@gmail.com>
2024-08-04 10:00:17 -0700

    test swapping clas

---
+++ src/test_4_august/Swap.java
@@ -0,0 +1,15 @@
+package test_4_august;
+
+public class Swap {
+
+    public static void main(String[] args) {
+// write a code to swapping two number a and b without using third variabls
+        //a+b=>a=a+b; 3+=5=>3=3+5=8
+      System.out.println("printing value of  a+b");
+
+
+
+    }
+
+
+}

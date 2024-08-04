5184e74a4a7e8db805da473129e061e425b2935f
Vartusha03 <svartika45@gmail.com>
2024-08-04 20:55:59 +0530

    code to check % of student

---
+++ src/test_4_august/StudentPercentage.java
@@ -0,0 +1,54 @@
+package test_4_august;
+
+public class StudentPercentage {
+
+    public static void main(String[] args) {
+
+        //write a code to check %of student
+        //case1: 33<=percentage<60
+
+        int percentage=45;
+
+        if(percentage>=33 && percentage<60) {
+            System.out.println("second division");
+        }
+        if(percentage>=60 && percentage<75) {
+            System.out.println("first divison");
+        }
+            if(percentage>75){
+                System.out.println("gold medalist");
+        }
+
+        percentage=70;
+
+        if(percentage>=33 && percentage<60) {
+            System.out.println("second division");
+        }
+            if(percentage>=60 && percentage<75) {
+                System.out.println("first divison");
+            }
+                if(percentage>75){
+                    System.out.println("gold medalist");
+
+        }
+
+        percentage=90;
+
+        if(percentage>=33 && percentage<60) {
+            System.out.println("second division");
+        }
+        if(percentage>=60 && percentage<75) {
+            System.out.println("first divison");
+        }
+        if(percentage>75){
+            System.out.println("gold medalist");
+
+        }
+
+    }
+
+    public static class LoginJavaClass {
+
+
+    }
+}

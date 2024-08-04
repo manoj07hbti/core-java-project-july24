236d3b463d07c4aa1b99808d646a2613947a9949
Himanshu <mannukumar9557@gmail.com>
2024-08-04 22:30:57 +0530

    swap number

---
+++ src/test_4_august/Swap_number.java
@@ -0,0 +1,28 @@
+package test_4_august;
+
+public class Swap_number {
+
+
+    public static void main(String[] args) {
+
+
+        int a = 20;
+        int b = 15;
+        System.out.println("before swapping a" + a);
+        System.out.println("before swapping b" + b);
+
+        a -= b;
+
+        System.out.println("after swapping a" + a);
+        System.out.println("after swaping a"+ b);
+        b += a;
+
+        System.out.println("after swapping b:" + b);
+        System.out.println("after swapping b:" + b);
+
+    }
+
+
+}
+
+

927cafebe3e54a0c390da0ec48117db83e3640a2
Vartusha03 <svartika45@gmail.com>
2024-08-06 11:40:45 +0530

    correction in balance check class

--- src/test_4_august/BalanceCheckClass.java
+++ src/test_4_august/BalanceCheckClass.java
@@ -5,49 +5,21 @@ public class BalanceCheckClass {
     public static void main(String[] args) {
 
         //case1:balance >10000
-        int balance = 120000;
+        //case2:5000<balance<10000
+        //case3:0<balance<5000
+
+        int balance =7000;
 
         if (balance > 0) {
             if (balance > 10000) {
                 System.out.println("please invest to FD");
-            }
-            if (balance > 5000 && balance < 10000) {
+            } else if (balance > 5000 && balance < 10000) {
                 System.out.println("total balance is:" + balance);
-            }
-            if (balance < 5000) {
+            } else if (balance < 5000) {
                 System.out.println("low balance:" + balance);
             }
-        }
-
-
-
-
-        //case2:5000<balance<10000
-
-        balance = 7000;
-        if (balance > 0) {
-            if (balance > 10000) {
-                System.out.println("please invest in FD");
-            }
-            if (balance > 5000 && balance < 10000) {
-                System.out.println("total balance is :" + balance);
-            }
-            if (balance>0 && balance < 5000) {
-                System.out.println("low balance:" + balance);
-            }
-        }
-         //case3:0<balance<5000
-        balance = 100;
-        if (balance > 0) {
-            if (balance > 10000) {
-                System.out.println("please invest in FD");
-            }
-            if (balance > 5000 && balance < 10000) {
-                System.out.println("total balance is :" + balance);
-            }
-            if (balance>0 && balance < 5000) {
-                System.out.println("low balance:" + balance);
-            }
+        }else {
+                System.out.println("please enter valid amount");
         }
     }
 }

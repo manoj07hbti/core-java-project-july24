ada7d5f59963bc92a23361f4dbc2e6dd5ed54fa3
Vartusha03 <svartika45@gmail.com>
2024-08-06 11:55:55 +0530

    correction in withdraw money class

--- src/test_4_august/WithdrawMoneyClass.java
+++ src/test_4_august/WithdrawMoneyClass.java
@@ -4,39 +4,21 @@ public class WithdrawMoneyClass {
 
     public static void main(String[] args) {
 
-      //write a code for withdrwa money
+      //write a code for withdraw money
         //case1)balance is multiple of 100
-        int balance=5000;
-
-        if(balance>0 && balance<10000){
-            if(balance%100==0){
-                System.out.println("valid amount:"+balance);
-            }else{
-                System.out.println("invalid amount:"+balance);
-            }
-        }
         //case2)not multiple of 100
-        balance=675;
+        //case3)balance is greater than 10000
+        int balance=1200;
 
         if(balance>0 && balance<10000){
             if(balance%100==0){
                 System.out.println("valid amount:"+balance);
             }else{
                 System.out.println("invalid amount:"+balance);
-            }
-        }
-
-        //case3)balance is greater than 10000
-        balance=12000;
-
-        if(balance>0 && balance<10000) {
-            if (balance % 100 == 0) {
-                System.out.println("valid amount:" + balance);
-            } else {
-                System.out.println("invalid amount:" + balance);
             }
         }else{
-                System.out.println("balance is greater than 10000:"+balance);
+            System.out.println("please enter valid number");
         }
+
     }
 }

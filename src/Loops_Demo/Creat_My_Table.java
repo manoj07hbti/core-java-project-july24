be88671a9b7be196cece9941d39458495c78d28f
bashu <imrankhankspcivil00786@gmail.com>
2024-08-08 18:59:50 +0530

    check my commet table for 5

---
+++ src/Loops_Demo/Creat_My_Table.java
@@ -0,0 +1,21 @@
+package Loops_Demo;
+
+public class Creat_My_Table {
+
+    public static void main(String[] args) {
+
+        //for(initialisation; condition;updation);
+
+        //SAYNTAX : creat the table for 5;
+
+        int number = 5;
+
+        System.out.println("enter the numbe of multipli table 5 ");
+
+        for(int k=1; k< 11; k++) {
+
+            System.out.println( 5 + " * " + k + " = " + (5 * k ) );
+        }
+
+    }
+}

213f62e3a0a88105356fd7d9117c2861d8ec6b95
hpRashmi <rashmi.ktdl@gmail.com>
2024-08-09 00:10:33 -0700

    loops Demoloops  table two five

---
+++ src/loops/DemoLoops.java
@@ -0,0 +1,21 @@
+package loops;
+
+public class DemoLoops {
+
+    public static void main(String[] args) {
+
+     //increment for loops
+
+
+        for (int i = 1; i <= 100; i++) {
+
+            System.out.println("even number" + i);
+        }
+
+      // decrement for loops
+
+        for(int j=1; j>=100; j--){
+            System.out.println("odd number"+j);
+        }
+    }
+}
---
+++ src/loops/TableOfTwoFive.java
@@ -0,0 +1,39 @@
+package loops;
+
+public class TableOfTwoFive {
+
+    public static void main(String[] args) {
+
+
+//  creat the table 2
+
+        int number=2;
+
+        for(int j=1; j <=10; j++){
+
+            System.out.println(2+"*"+j+"="+ (2 * j ));
+        }
+
+
+
+
+        //CREAT  THE TABLE 5
+
+         number=5;
+
+        for(int i=1; i<=10; i++){
+
+            System.out.println(5+"*"+i+"="+(5*i));
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}

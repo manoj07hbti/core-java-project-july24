9748684d9fc983443e65a6b66b23b8413721eacf
Himanshu <mannukumar9557@gmail.com>
2024-08-11 00:18:24 +0530

    while loops demo

---
+++ src/loops/While_loop.java
@@ -0,0 +1,79 @@
+package loops;
+
+public class While_loop {
+
+    public static void main(String[] args) {
+
+
+        //SYNTAX : SYNTAX:  while (condition){
+        //                                   //code
+        //			                 1 increment/decrement;
+        //                         }
+        int i=1;
+         while (i<=11){
+             System.out.println("NAMASTE WORLD.."+i);
+             i++;
+         }
+
+         //1DECREMENT
+         while (i>0){
+            System.out.println("DECREMENT NAMASTE WORLD.."+i);
+             i--;
+         }
+
+
+        //2 increment
+
+       long k=1;
+        while(k<=5){
+            System.out.println("HI WORLD..."+k);
+            k++;
+        }
+
+
+        //2 decrement;
+
+        while (k>0){
+            System.out.println("DECREMENT HI WORLD...."+k);
+            k--;
+        }
+
+      // 3 INCREMENT
+
+        int m=1;
+        while (m<=20){
+            System.out.println("hello java...."+m);
+            m++;
+        }
+
+        // 3 DECREMENT
+        while (m>0){
+            System.out.println("decrement hello java"+m);
+            m--;
+        }
+
+      // 4 INCREMENT
+
+       int l=10;
+        while (l<=50){
+            System.out.println("new hello world........."+l);
+            l++;
+        }
+        // 4 decrement
+
+        while (l >=10) {
+            System.out.println("decrement new hello world..."+l);
+            l--;
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+}

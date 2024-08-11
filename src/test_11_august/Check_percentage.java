2e532cffebdc3f08ad9addffacf1d9e8c493e56f
Himanshu <mannukumar9557@gmail.com>
2024-08-12 00:17:31 +0530

    PERCENTAGE

---
+++ src/test_11_august/Check_percentage.java
@@ -0,0 +1,22 @@
+package test_11_august;
+
+public class Check_percentage {
+
+
+    public static void main(String[] args) {
+
+
+        // percentage check
+
+        int number=500;
+        int percent=5;
+        int result= (int)(number*percent)/100;
+        System.out.println("PERCENTAGE OF PERSON.." +result);
+
+
+
+    }
+
+
+    }
+

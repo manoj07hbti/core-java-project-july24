945a2821bfc81e511c6eb0b874f38dca38446602
Himanshu <mannukumar9557@gmail.com>
2024-08-12 00:33:31 +0530

    even numbers code

---
+++ src/test_11_august/Even_number.java
@@ -0,0 +1,36 @@
+package test_11_august;
+
+public class Even_number {
+
+    public static void main(String[] args) {
+
+        // even number
+
+        int number=100;
+
+        for(int i=1; i<=100; i++){
+            if(i%2==0){
+                System.out.println("EVEN NUMBER.."+i);
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}

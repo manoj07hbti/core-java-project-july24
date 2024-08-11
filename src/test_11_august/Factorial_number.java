4393db29bbef5a466ff74f4bad81531fcc7226a5
Himanshu <mannukumar9557@gmail.com>
2024-08-12 00:12:46 +0530

    factorial

---
+++ src/test_11_august/Factorial_number.java
@@ -0,0 +1,14 @@
+package test_11_august;
+
+public class Factorial_number {
+
+    public static void main(String[] args) {
+
+
+        // factorial number
+
+        int i, fact = 1;
+        int number = 4;
+
+
+    }}

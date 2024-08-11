ce03d16747cf769c4f32a6a83156607038c2c2c0
Himanshu <mannukumar9557@gmail.com>
2024-08-12 00:27:04 +0530

    star pattern code

---
+++ src/test_11_august/Star_pattern.java
@@ -0,0 +1,46 @@
+package test_11_august;
+
+public class Star_pattern {
+
+
+    public static void main(String[] args) {
+
+
+
+
+        int number=5;
+        for(int i=1; i<=5; i++){
+            for (int j=1; j<=i; j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+
+
+        int num=1;
+        for(int i=5; i>=1; i--){
+            for (int j=1; j<=i; j++){
+                System.out.println("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

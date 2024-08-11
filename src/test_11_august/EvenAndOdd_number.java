adb0c9fe7bdcf4ac03f8f496ca17e777629c3e93
Himanshu <mannukumar9557@gmail.com>
2024-08-12 00:47:40 +0530

    even and odd numbers

---
+++ src/test_11_august/EvenAndOdd_number.java
@@ -0,0 +1,50 @@
+package test_11_august;
+
+public class EvenAndOdd_number {
+
+
+    //int number=300;
+    //
+    //        for(int i=3; i<=300; i++){
+    //            if(i%2==0){
+    //                System.out.println("EVEN NUMBERS.."+i);
+    //            }
+    //        }
+    //
+    //
+    //        number=300;
+    //
+    //        for(int j=3; j<=300; j++){
+    //            if(j%2!=0){
+    //                System.out.println("ODD NUMBERS..."+j);
+    //            }
+    //        }
+
+    public static void main(String[] args) {
+
+          //EVEN NUMBERS
+
+      int num=300;
+
+        for(int i=3; i<=300; i++){
+            System.out.println("even number"+i);
+        }
+
+       //ODD NUMBERS
+        num=300;
+        for(int h=3; h<=300; h++){
+            if(h%2!=0){
+                System.out.println("odd number"+h);
+            }
+        }
+
+
+    }
+
+
+
+
+
+
+
+}

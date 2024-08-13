2d5090360f5a0a3403a88b979ead62ea9753ec26
Vartusha03 <svartika45@gmail.com>
2024-08-13 12:34:31 +0530

    prime number class

--- src/test_11_august/PrimeNumbersClass.java
+++ src/test_11_august/PrimeNumbersClass.java
@@ -5,15 +5,21 @@ public class PrimeNumbersClass {
     public static void main(String[] args) {
 
 
-        //5)write a program to check prime numbers between 1 to 20
+        //10)write a program to check prime numbers between 1 to 20
 
-        int num = 10;
+        for (int number = 2; number <= 20; number++) {
+            boolean isPrime = true;
 
-        for (int i = 1; i <= 20; i++) {
-            if(num%num==0){
-                System.out.println("prime number:" + i);
+            for (int i = 2; i <number; i++) {
+                if (number % i == 0) {
+                    System.out.println("given number is divisible by other numbers so it is not prime:" + i);
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                System.out.println("************* NUMBER IS PRIME:" + number);
             } else {
-                System.out.println("not a prime number:" + i);
+                System.out.println("given number is not prime:" + number);
             }
         }
     }

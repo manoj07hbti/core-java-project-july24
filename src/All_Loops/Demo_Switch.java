d792e7a690459f5edeb8c6acf0841a06cb5c104f
bashu <imrankhankspcivil00786@gmail.com>
2024-08-14 23:08:09 +0530

    swith loop

---
+++ src/All_Loops/Demo_Switch.java
@@ -0,0 +1,58 @@
+package All_Loops;
+
+public class Demo_Switch {
+
+    public static void main(String[] args) {
+
+        int months = 12;
+
+        switch (months) {
+
+            case 1:
+                System.out.println("jan");
+                break;
+            case 2:
+                System.out.println("feb");
+                break;
+            case 3:
+                System.out.println("march");
+                break;
+            case 4:
+                System.out.println("april");
+                break;
+            case 5:
+                System.out.println("may");
+                break;
+            case 6:
+                System.out.println("june");
+                break;
+            case 7:
+                System.out.println("july");
+                break;
+            case 8:
+
+                System.out.println("august");
+                break;
+            case 9:
+
+                System.out.println("sept");
+                break;
+            case 10:
+
+                System.out.println("oct");
+                break;
+            case 11:
+
+                System.out.println("nov");
+                break;
+            case 12:
+
+                System.out.println("dec");
+                break;
+            default:
+                System.out.println("Invalid months found...");
+
+
+        }
+    }
+}

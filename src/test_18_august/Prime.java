9ac78cd0d2df5674d8146b47a6d855152ddbb57e
hpRashmi <rashmi.ktdl@gmail.com>
2024-08-21 03:32:35 -0700

    new

--- src/test_18_august/Prime.java
+++ src/test_18_august/Prime.java
@@ -7,19 +7,16 @@ public class Prime {
 
         // write a program to check prime numbers 15 to30
 
-      for(int number=2; number <=30; number++){
+      for(int number=15; number <=30; number++){
 
           boolean isPrime=true;
 
 
-          for(int i=15; i<=30;i++){
+          for(int i=15; i<number;i++){
 
               if(number%i==0) {
                   System.out.println("prime number:"+i);
-               for( i=2; i<number;i++){
 
-                   if(number %i==0){
-                       System.out.println("given number is divisible by other number is not prime"+i);
                        isPrime = false;
                    }
                }
@@ -45,10 +42,10 @@ public class Prime {
 
 
 
-    }
 
 
 
 
 
-}
+
+

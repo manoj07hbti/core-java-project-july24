9099a22e2eb19489fde582d76cb84c0332eceffc
hpRashmi <rashmi.ktdl@gmail.com>
2024-08-30 04:57:20 -0700

    test 25-august  practice

--- src/test_25_august/Even_Odd_Element.java
+++ src/test_25_august/Even_Odd_Element.java
@@ -6,7 +6,17 @@ public class Even_Odd_Element {
 
         int []arr={1,2,3,4,5,6};
 
-
+        int even = 0;
+        int odd = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("count of even number in an array:" + even);
+        System.out.println("count of odd number in an array:" + odd);
 
 
 

c3e4bf5bc111afb15e55fdb6a77f2862b9c2bf38
Himanshu <mannukumar9557@gmail.com>
2024-09-02 13:40:04 +0530

    method Practice

---
+++ src/methods/Method.java
@@ -0,0 +1,30 @@
+package methods;
+
+public class Method {
+
+    //SYNTAX : access_specifier return_type  method_name (parameter) {CODE..}
+
+    public void Squaremak(){
+
+        int i=85;
+
+        System.out.println("Square of given number: "+ i*i);
+
+    }
+
+    public static void main(String[] args) {
+
+        // STEM 1: CreateObject Using default Contractor
+
+        Method object1= new Method();
+
+        object1.Squaremak();
+
+
+    }
+
+
+
+
+
+}

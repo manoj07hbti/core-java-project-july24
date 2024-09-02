d6371d8b6c78bfce622756ad7e33d1ed71215566
hpRashmi <rashmi.ktdl@gmail.com>
2024-09-02 03:25:26 -0700

    method 2

---
+++ src/methods/Method2.java
@@ -0,0 +1,57 @@
+package methods;
+
+public class Method2 {
+
+    // check palindrome number
+
+    public void checkPalindrome() {
+        int number = 15251;
+        int remainder = 0;
+        int reversenumber = 0;
+        int originalnumber = number;
+        while (number > 0) {
+            remainder = number % 10;
+            reversenumber = reversenumber * 10 + remainder;
+            number = number / 10;
+        }
+        if (reversenumber == originalnumber) {
+            System.out.println("Getting palindrome number.." + originalnumber);
+        } else {
+            System.out.println("not geeting palindrome number..");
+        }
+    }
+
+    // check Armstrong number    153   // 1*1*1 + 5*5*5 + 3*3*3 = 153
+
+    public void checkArmstrong() {
+        int number = 153;
+        int result = 0;
+        int remainder = 0;
+        int originalnumber = number;
+        while (number != 0) {
+            remainder = number % 10;
+            result = (int) (result + Math.pow(remainder, 3));
+            number = number / 10;
+
+        }
+        if (originalnumber == result) {
+            System.out.println("Getting armstrong number..." + originalnumber);
+        } else {
+            System.out.println("Not getting number");
+        }
+    }
+
+    public static void main(String[] args) {
+        Method2 obj = new Method2();
+        obj.checkPalindrome();
+        obj.checkArmstrong();
+
+    }
+
+
+}
+
+
+
+
+

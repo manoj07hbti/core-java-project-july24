d1734bbbec72b42558671c0d06f13f979bc09240
Anshul152 <anshulkhan71@gmail.com>
2024-09-15 09:36:39 +0530

    ATM APPLICATION

--- src/scanner/Atm.java
+++ src/scanner/Atm.java
@@ -3,66 +3,94 @@ package scanner;
 import java.util.Scanner;
 
 public class Atm {
-
-    public void createAtm() {
+    public String cashwithdrawl() {
+        String result = " ";
         Scanner scanner = new Scanner(System.in);
-        System.out.println("WELCOME Sir/Madam..");
-        String name = scanner.nextLine();
-        System.out.println("Welcome to SBI bank ATM..");
-
-        int pinnumber = 3265;
-        if (pinnumber == 3265) {
-            System.out.println("please enter your valid pin");
-
-            Scanner scanner2 = new Scanner(System.in);
-            pinnumber = scanner2.nextInt();
-        }
-        int balance = 100000, withdraw, deposit;
-        Scanner scanner1 = new Scanner(System.in);
-        while (true) {
-            System.out.println("Choose 1 for withdraw");
-            System.out.println("Choose 2 for deposit");
-            System.out.println("Choose 3 for check balance");
-            System.out.println("Choose 4 to exit ");
-            System.out.println("Choose the operation you want to perform");
-            int number = scanner.nextInt();
-            switch (number) {
-                case 1:
-                    System.out.println("Enter the money to be withdrawn");
-                    withdraw = scanner.nextInt();
-                    if (balance >= withdraw) {
-                        balance = balance - withdraw;
-                        System.out.println("collect your money");
-                    } else {
-                        System.out.println("Insufficient balance");
-                    }
-                    System.out.println("");
-                    break;
-
-                case 2:
-                    System.out.println("Enter money to deposit..");
-                    deposit = scanner.nextInt();
-                    balance = balance + deposit;
-                    System.out.println("Money is successfully deposited");
-                    System.out.println("");
-                    break;
-
-                case 3:
-                    System.out.println("Balance: " + balance);
-                    System.out.println("");
-                    break;
-
-                case 4:
-                    System.exit(0);
-
+        System.out.println("enter the cash withdrawl amount:");
+        int balance = 100000;
+        int amount = scanner.nextInt();
+        if (balance > amount) {
+            if (amount % 100 == 0) {
+                balance = balance - amount;
+                result = "your cash withdrawl amount is: " + amount + " and your remaning balance is: " + balance;
+            } else {
+                result = "insufficient balance.." + balance;
             }
 
+        }
+        return result;
+    }
+
+    public String balancecheck() {
+        String result = " ";
+        double balance = 10025.3;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("this is your updated balance:" + balance);
+        return result;
+    }
+
+    public String deposit() {
+        String result = " ";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("please enter deposit amount");
+        double balance = 10025.3;
+        double amount = scanner.nextDouble();
+        balance = amount + balance;
+        System.out.println("deposited successfully: total avaliable balance is:" + balance);
+        return result;
+    }
+
+    public String exist() {
+        String result = " ";
+        Scanner scanner = new Scanner(System.in);
+        result = "exist from menu, thank uou for using SBI service..";
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("welcome to sbi bank");
+
+        System.out.println("enter your pin...");
+        int pin = 3214;
+        int p = scanner.nextInt();
+        if (p == 3214) {
+            System.out.println("pin is correct...");
+        } else {
+            System.out.println("please enter valid pin...your pin is invalid.");
+        }
+        scanner = new Scanner(System.in);
+        System.out.println("please choose an option...1)cash withdrawl, 2)balance check, 3)deposit, 4)exit");
+
+        String re_choice = "Y";
+        while (re_choice.equals("Y")) {
+            scanner = new Scanner(System.in);
+
+            Scanner scanner_1 = new Scanner(System.in);
+            int choice = scanner_1.nextInt();
+            Atm obj = new Atm();
+
+
+            if (choice == 1) {
+                //call cash withdrawl
+                System.out.println(obj.cashwithdrawl());
+            } else if (choice == 2) {
+                //call balance check
+                System.out.println(obj.balancecheck());
+            } else if (choice == 3) {
+                //call deposit
+                System.out.println(obj.deposit());
+            } else if (choice == 4) {
+                //call exist
+                System.out.println(obj.exist());
+            }
+
+            System.out.println("Do you want to continue press Y and and please choose an option...1)cash withdrawl, 2)balance check, 3)deposit, 4)exit...");
+            Scanner scanner_2 = new Scanner(System.in);
+            re_choice = scanner_2.nextLine();
 
         }
     }
-
-    public static void main(String[] args) {
-        Atm obj = new Atm();
-        obj.createAtm();
-    }
 }
+

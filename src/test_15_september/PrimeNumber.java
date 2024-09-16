6865f0b6c7073917f5f5ab8681b6f45bfecd4e47
Anshul152 <anshulkhan71@gmail.com>
2024-09-16 19:04:09 +0530

    TEST 15 SEPTEMBER

---
+++ src/test_15_september/PrimeNumber.java
@@ -0,0 +1,46 @@
+package test_15_september;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public void checkPrime() {
+        int number = 0;
+        System.out.println("please enter a number");
+        Scanner scanner = new Scanner(System.in);
+        number = scanner.nextInt();
+        boolean isPrime = true;
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                System.out.println("Number divisible by other number hence it is not prime" + number);
+                isPrime = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String rechoice = "Y";
+        Scanner scanner = new Scanner(System.in);
+        PrimeNumber obj = new PrimeNumber();
+        System.out.println("Do you want to find more prime number press Y and if you have to exit press any key");
+        String choice =
+                scanner.nextLine();
+
+        if (choice.equalsIgnoreCase("Y")) {
+            obj.checkPrime();
+            while (rechoice.equalsIgnoreCase("Y")) {
+                System.out.println("Do you want to find more prime number press Y and if you have to exit press any key");
+                rechoice = scanner.next();
+                if (rechoice.equalsIgnoreCase("Y")) {
+                    obj.checkPrime();
+                } else {
+                    System.out.println("EXit");
+                }
+            }
+        }
+
+    }
+}
+
+
+

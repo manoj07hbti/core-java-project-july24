ad114969f01724cf6042c9a6e4923418283ca393
Himanshu <mannukumar9557@gmail.com>
2024-09-16 21:06:47 +0530

    collection assignment

--- src/collections/DemoCollection.java
+++ src/collections/DemoCollection.java
@@ -1,6 +1,23 @@
 package collections;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class DemoCollection {
+
+    public static void main(String[] args) {
+
+        // I want to store 10 country names
+
+        // Array List
+
+        ArrayList country= new ArrayList(); // we have a  created list
+         // adding data to list : objectName.add(data)
+
+
+
+
+    }
 
     
 }

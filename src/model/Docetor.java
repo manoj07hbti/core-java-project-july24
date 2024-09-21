0219ecbd7f927056da09c16ccabc3cfa414cd009
hpRashmi <rashmi.ktdl@gmail.com>
2024-09-21 08:02:59 -0700

    model

---
+++ src/model/Docetor.java
@@ -0,0 +1,56 @@
+package model;
+
+public class Docetor {
+
+    String name;
+    int age;
+    String speciality;
+    String degree;
+
+
+    // param constructor
+
+
+    public Docetor(String name, int age, String speciality, String degree) {
+        this.name = name;
+        this.age = age;
+        this.speciality = speciality;
+        this.degree = degree;
+
+
+    }
+    // getter setter
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getSpeciality() {
+        return speciality;
+    }
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+    public String getDegree() {
+        return degree;
+    }
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+}
+
+
+
---
+++ src/model/Employed.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Employed {
+
+    public class Employee {
+        String name;
+        int age;
+        String department;
+        // parametrized constructor
+        public Employee(String name, int age, String department) {
+            this.name = name;
+            this.age = age;
+            this.department = department;
+        }
+        // getter setter
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+        public int getAge() {
+            return age;
+        }
+        public void setAge(int age) {
+            this.age = age;
+        }
+        public String getDepartment() {
+            return department;
+        }
+        public void setDepartment(String department) {
+            this.department = department;
+        }
+    }
+
+}

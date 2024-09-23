0f0c1a6f2725dddbb91731b00e520848af7e9d4b
BLADEZY <mrbladezy@gmail.com>
2024-09-23 12:09:50 +0530

    Changes

--- src/Methods/Employee.java
+++ src/Methods/Employee.java
@@ -4,15 +4,15 @@ public class Employee
 {
     public void work()
     {
-        System.out.println("You're printing working method in the Employee class...");
+        System.out.println("You're printing working method in the Engineer class...");
     }
     public void eat()
     {
-        System.out.println("You are printing eating method in the Employee class...");
+        System.out.println("You are printing eating method in the Engineer class...");
     }
     public void sleep()
     {
-        System.out.println("You are printing sleeping method in the Employee class...");
+        System.out.println("You are printing sleeping method in the Engineer class...");
     }
 
     public static void main(String[] args)

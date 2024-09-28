10f05ecfb8c834616d5fca4b07238ad2857dfa11
hpRashmi <rashmi.ktdl@gmail.com>
2024-09-28 01:55:29 -0700

    SET HASHSET DOCTOR ENGINEER STUDENT

---
+++ src/collection/set/DemoDocetorHashSet.java
@@ -0,0 +1,46 @@
+package collection.set;
+
+import model.Docetor;
+
+
+import java.util.HashSet;
+
+public class DemoDocetorHashSet {
+
+    public static void main(String[] args) {
+
+
+
+    HashSet<Docetor> doctor = new HashSet<>();
+
+    Docetor doctor1 = new Docetor("rashmi", 31, "TUNDLA", "IT");
+
+        System.out.println("HAshocde of docetor1...." + doctor1.hashCode());
+
+
+    Docetor doctor2= new Docetor("RASHMI", 31, "TUNDLA", "IT");
+        System.out.println("Hashcode of docetor2..." + doctor2.hashCode());
+
+
+    Docetor docetor3 = new Docetor("ADEEBA", 25, "TUNDLA", "MBBS");
+        System.out.println("Hashcode os doctor3..." +docetor3.hashCode());
+
+
+    Docetor docetor4 = new Docetor("ADEEBA", 26, "TUNDLA", "MBBS");
+        System.out.println("Hashcode of docetor4 " + docetor4.hashCode());
+
+
+        doctor.add(doctor1);
+        doctor.add(doctor2);
+        doctor.add(docetor3);
+        doctor.add(docetor4);
+
+        for (Docetor var : doctor) {
+
+        System.out.println(" Name. " + var.getName() + "  Age " + var.getAge() + " SPECIALITY " + var.getSpeciality() + " DEGREE " + var.getDegree());
+    }
+
+
+    }
+}
+
---
+++ src/collection/set/DemoEngineerHashSet.java
@@ -0,0 +1,38 @@
+package collection.set;
+
+import model.Engineer;
+
+import java.util.HashSet;
+
+public class DemoEngineerHashSet {
+
+    public static void main(String[] args) {
+        HashSet<Engineer> engineers = new HashSet<>();
+
+        Engineer engineer1 = new Engineer("rashmi", 31, "TUNDLA", "IT");
+        System.out.println("HAshocde of engineer1...." + engineer1.hashCode());
+
+
+        Engineer engineer2 = new Engineer("RASHMI", 31, "TUNDLA", "IT");
+        System.out.println("Hashcode of engineer2..." + engineer2.hashCode());
+
+
+        Engineer engineer3 = new Engineer("ADEEBA", 25, "TUNDLA", "MBBS");
+        System.out.println("Hashcode os engineer3..." + engineer3.hashCode());
+
+
+        Engineer engineer4 = new Engineer("ADEEBA", 26, "TUNDLA", "MBBS");
+        System.out.println("Hashcode of engineer4 " + engineer4.hashCode());
+
+
+        engineers.add(engineer1);
+        engineers.add(engineer2);
+        engineers.add(engineer3);
+        engineers.add(engineer4);
+
+        for (Engineer var : engineers) {
+
+            System.out.println(" Name. " + var.getName() + "  Age " + var.getAge() + " City " + var.getCity() + " Branch " + var.getBranch());
+        }
+    }
+}

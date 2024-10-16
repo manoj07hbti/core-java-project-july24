692ec7099b164ceda25230d10f91698ff808e983
Anshul152 <anshulkhan71@gmail.com>
2024-10-16 09:35:09 +0530

    OOPS INHERITANCE DAY 1

---
+++ src/oops/inheritance/DemoA.java
@@ -0,0 +1,15 @@
+package oops.inheritance;
+
+import java.sql.SQLOutput;
+
+public class DemoA {
+
+    public void method1(){
+
+        System.out.println("This is method1 for DemoA class");
+        System.out.println("Hello world...");
+
+    }
+
+
+}
---
+++ src/oops/inheritance/DemoB.java
@@ -0,0 +1,15 @@
+package oops.inheritance;
+
+public class DemoB extends DemoA {
+
+
+    public static void main(String[] args) {
+
+        DemoB demoB=new DemoB();
+        demoB.method1();
+
+    }
+
+
+
+}

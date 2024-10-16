3cdd4edaedf9736e9601f819056a80604ea3426f
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-10-16 09:01:42 +0530

    OOPS: Day1

---
+++ src/oops/inheritance/DemoA.java
@@ -0,0 +1,12 @@
+package oops.inheritance;
+
+public class DemoA {
+
+    protected void m1(){
+        System.out.println("This is M1 method of DemoA CLASS...");
+    }
+
+    protected void m2() {
+        System.out.println("This is M2 method DemoA CLASS");
+    }
+}
---
+++ src/oops/inheritance/DemoB.java
@@ -0,0 +1,14 @@
+package oops.inheritance;
+
+// SINGLE LEVEL INHERITANCE
+public class DemoB extends DemoA{
+    protected void m3(){
+        System.out.println("This is M3 method DemoB CLASS");
+    }
+
+    public static void main(String[] args) {
+        DemoB demoB= new DemoB();
+        demoB.m1();// we are calling parent class method
+        demoB.m2();
+    }
+}

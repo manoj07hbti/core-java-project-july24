f714fd0c18aa524d671db4c01cce6799822e1145
hpRashmi <rashmi.ktdl@gmail.com>
2024-10-17 10:31:19 -0700

    opps.inheritance new

--- src/opps/inheritance/GooglePay.java
+++ src/opps/inheritance/GooglePay.java
@@ -1,11 +1,19 @@
 package opps.inheritance;
 
-public class GooglePay   extends  PaymentGateway{
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-        
+public class GooglePay   extends  PaymentGateway {
+
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void googlePay() {
+        System.out.println("Printing google pay transaction providing QR code  , Air tickets , ");
+    }
+        public static void main (String[]args){
+
             GooglePay obj = new GooglePay();
-            
+
             obj.googlePay();
         }
 

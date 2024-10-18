653ce82e59ab8d3fbb0fc2f41dfe6df0af64e74d
Manoj Kumar <manojkumar.pcf@gmail.com>
2024-10-18 09:26:04 +0530

    OOPS: Day3

---
+++ src/oops/inheritance/abstraction/DemoAbstraction.java
@@ -0,0 +1,23 @@
+package oops.inheritance.abstraction;
+
+public abstract class DemoAbstraction {
+
+    //TODO Abstract method : a method without method body
+    //access_specifier abstract return_type method_name(input param);
+    public abstract void m1();// Abstract method
+
+    //TODO Abstract class: any class which has at least one abstract method is called as Abstract class
+    public void m2(){
+        System.out.println("This is M2 method ");
+    }
+    //50 % abstraction achieved here hence
+    // TODO Abstract classes provides partial abstraction as we also have normal methods in a abstract class
+    //TODO Q: can we  create object of Abstract class? if No then why ?
+    public static void main(String[] args) {
+        /* DemoAbstraction obj= new DemoAbstraction();// No we can not create object of Abstract class
+        obj.m1(); */
+        /*TODO Ans: As abstract method do not have any code to execute so JVM will not be able to execute anything so java do not
+           Support creation of object for a Abstract class */
+    }
+
+}
---
+++ src/oops/inheritance/abstraction/DemoInterface.java
@@ -0,0 +1,11 @@
+package oops.inheritance.abstraction;
+
+public interface DemoInterface {
+
+    //TODO it provides 100% abstraction
+    public void m1();
+    public void m2();
+    public void m3();
+    // TODO : interface always provides 100% abstraction as it do not have any normal method
+
+}

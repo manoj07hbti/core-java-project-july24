8ab8bbdba5993536160bb0823f9cc606d77614c9
hpRashmi <rashmi.ktdl@gmail.com>
2024-10-31 02:42:17 -0700

    27-0ct-test-pr

---
+++ src/test_27_oct/Student.java
@@ -0,0 +1,50 @@
+package test_27_oct;
+
+public class Student {
+
+
+    String name;
+    int age;
+    String city;
+    String dept;
+
+
+    public Student(String name, int age, String city, String dept) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+        this.dept = dept;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDept() {
+        return dept;
+    }
+
+    public void setDept(String dept) {
+        this.dept = dept;
+    }
+}
